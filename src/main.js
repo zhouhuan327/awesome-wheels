beaa968438298185b4f43b772180890e2ee1437e
sean <zsean21598@163.com>
2020-05-20 15:03:06 +0800

    使用chai编写测试用例

--- src/main.js
+++ src/main.js
@@ -8,3 +8,46 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+//单元测试
+import Button from '@/components/Button/button.vue'
+import chai from 'chai'
+const expect = chai.expect;
+{  //测试icon
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData: {
+      icon: 'setting'
+    }
+  })
+  button.$mount()
+  const href = button.$el.querySelector('use').getAttribute('xlink:href')
+  expect(href).to.eq('#icon-setting')
+}
+{ //测试loading
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData: {
+      icon: 'setting',
+      loading: true
+    }
+  })
+  button.$mount()
+  const href = button.$el.querySelector('use').getAttribute('xlink:href')
+  expect(href).to.eq('#icon-loading')
+
+}
+{
+  const Constructor = Vue.extend(Button)
+  const zbutton = new Constructor({
+    propsData: {
+      icon: 'setting',
+    }
+  })
+  zbutton.$mount()
+  zbutton.$on('click', function (e) {
+    console.log(e)
+  })
+  console.log(zbutton.$el)
+  zbutton.$el.click()
+}
