3f47f66648e49688f53980260f1196fe8c57ee43
sean <zsean21598@163.com>
2020-05-21 16:24:57 +0800

    准备实现栅格系统

---
+++ src/components/grid/Col.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="col">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'z-col',
+  data() {
+    return {
+
+    }
+  }
+ }
+</script>
+
+<style lang="" scoped>
+  .col{
+
+  }
+</style>
---
+++ src/components/grid/Row.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'z-row',
+  data() { 
+    return {
+
+    }
+  }
+ }
+</script>
+
+<style lang="scss" scoped>
+  .row{
+      display: flex;
+  }
+</style>
