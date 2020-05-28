f8cf3a193651fdf2d90caf79c3710916f8451d42
sean <zsean21598@163.com>
2020-05-28 17:50:39 +0800

    add prop position

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
 <template>
 	<div id="app">
-		<z-popover>
+		<z-popover position="top">
 			<template slot="content">
-				dd
+				toptoptoptoptoptoptop
 			</template>
-			<z-button @click="clickEvent" icon="setting">默认按钮</z-button>
+			<z-button icon="setting">top</z-button>
 		</z-popover>
+		<z-popover position="bottom">
+			<template slot="content">
+				bottombottombottombottombottom
+			</template>
+			<z-button icon="setting">bottom</z-button>
+		</z-popover>
+		<z-popover position="left">
+			<template slot="content">
+				left
+			</template>
+			<z-button icon="setting">left</z-button>
+		</z-popover>
+		<z-popover position="right">
+			<template slot="content">
+				right
+			</template>
+			<z-button icon="setting">right</z-button>
+		</z-popover>
+<!--		<z-button @click="clickEvent" icon="setting">默认按钮</z-button>-->
 <!--		<z-input value="12" disabled />-->
 <!--		<z-input @change="change" error="出错了" value="1233" />-->
 <!--		<z-input suffixIcon="setting" v-model="message" />-->
@@ -114,6 +133,7 @@ export default {
 	-moz-osx-font-smoothing: grayscale;
 	display: flex;
 	align-items: center;
+	justify-content: center;
 	color: #2c3e50;
 	margin-top: 20px;
 }
--- src/components/Popover/Popover.vue
+++ src/components/Popover/Popover.vue
@@ -1,11 +1,14 @@
 <template>
     <div class="z-popover" @click="onClickTrigger">
         <!--        触发的元素-->
-        <span ref="trigger">
+        <span ref="trigger" style="display: inline-block">
             <slot></slot>
         </span>
         <!--        弹出的内容-->
-        <div ref="content" v-if="isVisible" class="z-popover-content">
+        <div ref="content" v-if="isVisible"
+             class="z-popover-content"
+             :class="{[`position-${position}`]:true}"
+        >
             <slot name="content"></slot>
         </div>
 
@@ -20,17 +23,41 @@
                 isVisible:false
             }
         },
-        mounted() {
-
+        props:{
+            position:{
+                type:String,
+                default:'top',
+                validator(value) {
+                    return ['top','left','right','bottom'].includes(value)
+                }
+            }
         },
         methods:{
             movePostion(){
                 const {content} = this.$refs;
                 document.body.appendChild(content)
-                const {top,left} = this.$refs.trigger.getBoundingClientRect()
-                const {width,height} = getComputedStyle(content)
-                this.$refs.content.style.left = left + 'px'
-                this.$refs.content.style.top = (top - parseInt(height)/2)   + 'px'
+                const {top,left,width,height} = this.$refs.trigger.getBoundingClientRect()
+
+                switch (this.position) {
+                    case "top":
+                        content.style.left = left - width  + 'px'
+                        content.style.top = top + 'px'
+                        break
+                    case "bottom":
+                        content.style.left = left - width + 'px'
+                        content.style.top = top + height + 'px'
+                        break
+                    case "left":
+                        content.style.left = left + 'px'
+                        content.style.top = top + 'px'
+                        break
+                    case "right":
+                        content.style.left = left + width + 'px'
+                        content.style.top = top + 'px'
+                        break
+
+                }
+
             },
             onClickDocument(e){
                 const {content,trigger} = this.$refs;
@@ -40,7 +67,6 @@
                         this.isVisible = false;
                     }
                 }
-
             },
             open(){
                 this.isVisible = true
@@ -69,6 +95,7 @@
 </script>
 
 <style scoped lang="scss">
+    $popover-bg-color:#fff;
     .z-popover{
         display: inline-block;
         position: relative;
@@ -76,14 +103,64 @@
     .z-popover-content{
         display: inline-block;
         position: absolute;
-        min-width: 150px;
+        min-width: 100px;
         min-height: 30px;
+        max-width: 15em;
+        word-break: break-all;
         text-align: center;
-        left: 0;
-        bottom: 100%;
         box-shadow:0 2px 12px 0 rgba(0,0,0,.1);
-        padding:10px 5px;
-        transform: translateY(-100%);
+        background:$popover-bg-color;
+        padding:0.5em 1em;
+        &::before{
+            content: '';
+            display: block;
+            width: 10px;
+            height: 10px;
+            position: absolute;
+
+        }
+        &.position-top{
+            transform: translateY(-100%);
+            margin-top: -12px;
+            &::before{
+                top: 100%;
+                left: 50%;
+                transform: translateX(-50%);
+                border: 10px solid transparent;
+                border-top-color: $popover-bg-color;
+            }
+        }
+        &.position-bottom{
+            margin-top: 12px;
+            &::before{
+                bottom: 100%;
+                left: 50%;
+                transform: translateX(-50%);
+                border: 10px solid transparent;
+                border-bottom-color: $popover-bg-color;
+            }
+        }
+        &.position-left{
+            transform: translateX(-100%);
+            margin-left: -12px;
+            &::before{
+                top: 50%;
+                left: 100%;
+                transform: translateY(-50%);
+                border: 10px solid transparent;
+                border-left-color: $popover-bg-color;
+            }
+        }
+        &.position-right{
+            margin-left: 12px;
+            &::before{
+                top: 50%;
+                right: 100%;
+                transform: translateY(-50%);
+                border: 10px solid transparent;
+                border-right-color: $popover-bg-color;
+            }
+        }
     }
 
 </style>
