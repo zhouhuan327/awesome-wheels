8e59e45d574149029ea3989030f287e3fffae420
sean <zsean21598@163.com>
2020-05-30 13:13:39 +0800

    修复无法打包的问题

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import Button from './components/Button/button.vue'
 import ButtonGroup from './components/Button/button-group.vue'
 import Icon from './components/icon.vue'
 import Row from './components/grid/Row'
-import Col from "@/components/grid/Col";
-import NoticePlugin from "@/components/notice/NoticePlugin";
-import Popover from "@/components/Popover/Popover";
+import Col from "./components/grid/Col";
+import NoticePlugin from "./components/notice/NoticePlugin";
+import Popover from "./components/Popover/Popover";
 
 export { Button,ButtonGroup,Icon,Row,Col,NoticePlugin,Popover }
--- vue.config.js
+++ vue.config.js
@@ -14,15 +14,16 @@ module.exports = {
         }
     },
     configureWebpack: config => {
-        Object.assign(config, {
-            // 开发生产共同配置
-            resolve: {
-                extensions: [".js", ".vue", ".json"],
-                alias: {
-                    vue$: "vue/dist/vue.js",
-                    "@": path.resolve(__dirname, "./src")
-                }
-            }
-        });
+        // if (process.env.NODE_ENV === 'development') {
+        //     // 为生产环境修改配置...
+        //     config.resolve = {
+        //         extensions: [".js", ".vue", ".json"],
+        //             alias: {
+        //             vue$: "vue/dist/vue.js",
+        //                 "@": path.resolve(__dirname, "./src")
+        //         }
+        //     }
+        // }
     }
+
 }
