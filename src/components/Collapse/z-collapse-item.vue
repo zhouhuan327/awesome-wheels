709fad8f09f1a44643b7e37f1eb129ab6ba04a4a
sean <zsean21598@163.com>
2020-06-22 18:32:12 +0800

    add collapse animation

--- src/components/Collapse/z-collapse-item.vue
+++ src/components/Collapse/z-collapse-item.vue
@@ -3,9 +3,11 @@
         <div class="z-collapse-item-head" @click="toggle">
             {{title}}
         </div>
-        <div class="z-collapse-item-content" v-if="active ">
-            <slot></slot>
-        </div>
+        <transition name="slide" >
+            <div class="z-collapse-item-content" :class="active?'active':''">
+               <slot></slot>
+            </div>
+        </transition>
     </div>
 </template>
 
@@ -56,7 +58,14 @@
             user-select: none;
         }
         .z-collapse-item-content{
-            padding: 16px 8px;
+            overflow: hidden;
+            max-height: 0;
+            transition: all 0.3s ease;
+            padding-left: 15px;
+            &.active{
+                padding: 15px;
+                max-height: 50px;
+            }
         }
     }
 </style>
